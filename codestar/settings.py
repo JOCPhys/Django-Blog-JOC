93c684727e107b4301c16f6bc2c7afad8e84d4e2
Joanna O'Connor <jtoconnor75@gmail.com>
2024-12-20 14:50:27 +0000

    create superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%fp%t32ox+c7@lxad()^n&79wn$6czzbdi%8b%znpn@ofrnze-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
