d2d9646aa0ea7785c58cb374e6df596b9c409e58
Joanna O'Connor <jtoconnor75@gmail.com>
2025-01-07 18:17:46 +0000

    set debug = false

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', 'django-blog-joc-ced4343d79e2.herokuapp.com']
 
